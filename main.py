e89bdaf516213d2d7f1b8d7fa6443870b0fa67a1
mildredit <128537823+mildredit@users.noreply.github.com>
2023-04-17 10:46:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("fef")
+from arithmetic_arranger import arithmetic_arranger 
+from unittest import main
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print (" ")
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+print(" ")
+
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]))
+print(" ")
+
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
+main(module='test_module', exit=False)
